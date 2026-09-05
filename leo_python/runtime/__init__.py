62efe9bf8a8605ddedbc0dfbb9262ac6ad296c49
Abshaykat <absiddshaykat@gmail.com>
2026-09-06 02:53:32 +0600

    feat: expose bounded task runtime primitives

--- leo_python/runtime/__init__.py
+++ leo_python/runtime/__init__.py
@@ -2,3 +2,5 @@ from .controlled_executor import ControlledExecutor, ExecutionResult
 from .capability_runner import CapabilityRunRequest, CapabilityRunner
 from .recovery import RecoveryPolicy, RecoveryResult
 from .safe_capability import register_safe_echo
+from .task import TaskRecord, TaskState
+from .task_manager import TaskManager
