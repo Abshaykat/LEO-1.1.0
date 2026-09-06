de3bdbecc89b5e73cbcb5ff55c2ff0898364c551
Abshaykat <absiddshaykat@gmail.com>
2026-09-06 08:40:14 +0600

    security: expose Mark governance guard and capability manifest

--- leo_python/integrations/__init__.py
+++ leo_python/integrations/__init__.py
@@ -1,2 +1,4 @@
 from .mark_adapter import MarkCapabilityAdapter, MarkAdapterRegistry
 from .mark_policy import MarkIntegrationPolicy
+from .mark_guard import MarkGuard
+from .mark_manifest import MarkCapabilitySpec
